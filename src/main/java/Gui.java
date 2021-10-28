86e6bef0ba50e585f39cc284c554ef007ea8615b
F3f0 <federico.barberi@gmail.com>
2021-10-28 17:13:35 +0200

    Adderat style step 2 med bild till vinnaren

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -6,8 +6,11 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.AttributedCharacterIterator;
+import java.util.Map;
 
-    class GUI extends JFrame implements ActionListener  {
+class Gui extends JFrame implements ActionListener  {
+
     JPanel controls = new JPanel();
     JPanel gameBoard = new JPanel();
     JButton newGame = new JButton("New Game");
@@ -15,7 +18,7 @@ import java.io.IOException;
     JLabel pic = new JLabel(new ImageIcon(myPicture));
 
 
-    GUI() throws IOException {
+    Gui() throws IOException, FontFormatException {
 
         setLayout(new GridLayout(2,1));
         add(gameBoard);
@@ -24,6 +27,7 @@ import java.io.IOException;
         JButton []  arrayButton = new JButton[buttons*buttons];
         gameBoard.setLayout(new GridLayout(buttons, buttons));
 
+
         for(int i =0 ; i < buttons*buttons; i++){
             JButton button = new JButton(Integer.toString(i));
             arrayButton[i] = button;
@@ -31,6 +35,7 @@ import java.io.IOException;
             button.setForeground(Color.BLACK);
             button.setBackground(Color.MAGENTA);
             button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+            button.setFont(Gui.scaryFont());
             button.setBorderPainted(true);
             button.addActionListener( this);
         }
@@ -38,17 +43,30 @@ import java.io.IOException;
         controls.setBackground(Color.WHITE);
         controls.add(pic);
         newGame.addActionListener(this);
+        newGame.setFont(Gui.scaryFont());
         newGame.setBackground(Color.orange);
         newGame.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         setLocation(400,100);
         setSize(400,500);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        Gui.winningPic();
     }
 
-
-
+    static public void winningPic(){
+        JDialog dialog = new JDialog();
+        JLabel label = new JLabel( new ImageIcon("src/Treisman-Scary-Stories.png") );
+        dialog.add(label);
+        dialog.setLocation(300,25);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+    static Font scaryFont() throws IOException, FontFormatException {
+        Font scaryFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Halloween Morning.ttf")).deriveFont(24f);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(scaryFont);
+        return scaryFont;
+    }
 
     @Override
     public void actionPerformed(ActionEvent event) {
@@ -57,6 +75,4 @@ import java.io.IOException;
         if(event.getSource() == newGame)
             System.out.println("NewGame");
     }
-
-
 }
